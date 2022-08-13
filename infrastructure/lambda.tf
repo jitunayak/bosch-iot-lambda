809c4380fd5faf2312844582cec5091ca3b88bf6
jitunayak <jitunayak715@gmail.com>
2022-08-13 21:02:50 +0530

    added code hash to deploy latest code

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -24,13 +24,14 @@ data "archive_file" "zip_nodejs" {
   output_path = "../deploy/lambda-deploy.zip"
 }
 resource "aws_lambda_function" "lambda_function" {
-  filename      = "../deploy/lambda-deploy.zip"
-  function_name = "bosch-iot-lambda"
-  description   = "Store IOT device data"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = aws_iam_role.bosch_iot_lambda_role.arn
-  memory_size   = 128
+  filename         = "../deploy/lambda-deploy.zip"
+  function_name    = "bosch-iot-lambda"
+  description      = "Store IOT device data"
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  source_code_hash = filebase64sha256(data.archive_file.zip_nodejs.output_path)
+  role             = aws_iam_role.bosch_iot_lambda_role.arn
+  memory_size      = 128
   tags = {
     Name = "bosch_iot_lambda"
   }
